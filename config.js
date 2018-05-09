79bf17f26e1c30651a33e1c0df74b4c1be8fada6
Pushpendra Rajput <prerna.g@dimensionindia.com>
2018-05-09 13:33:45 +0530

    login logo

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ configuration = {
     'projectName': 'GEMLS Shipping Services',
     'imageDir': 'images',
     'logo':'logo.png',
+    'loginLogo':'login-logo.png',
     'copyright':'Copyright 2018 GEMLS | All Rights Reserved.',
     'statusArr':{'A':'Active', 'I':'InActive'},
     'uploadTempDir':'public/tmp/',
